4939a689e6700851ae09a7b7b7df237b1067e5c2
Nazmul Huda <mhnazmul48@gmail.com>
2020-10-09 22:58:13 +0600

    UI Done

--- src/Components/Home/Bannar/Bannar.js
+++ src/Components/Home/Bannar/Bannar.js
@@ -9,7 +9,7 @@ const Bannar = () => {
                     <div className="col-md-4 offset-md-1">
                         <h1 className='font-weight-bolder textColor2'>Your New Smile <br/> Starts Here</h1>
                         <p className='lead'>Vero officia atque ipsa, quo et architecto omnis veniam quos magnam id! Lorem ipsum dolor sit amet.</p>
-                        <button className='btn bgGradient'>GET APPOINMENT</button>
+                        <button className='btn bgGradient text-light'>GET APPOINMENT</button>
                     </div>
                     <div className="col-md-6">
                         <img className='img-fluid' src={bannar} alt=""/>
--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Appoinment from './Appoinment/Appoinment';
+import Blog from './Blog/Blog';
+import Contract from './Contract/Contract';
+import Doctors from './Doctors/Doctors';
+import FeaturedService from './FeaturedService/FeaturedService';
+import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Service from './Service/Service';
+import Testimonial from './Testimonial/Testimonial';
 
 const HomePage = () => {
     return (
         <>
             <Header></Header>
             <Service></Service>
+            <FeaturedService></FeaturedService>
+            <Appoinment></Appoinment>
+            <Testimonial></Testimonial>
+            <Blog></Blog>
+            <Doctors></Doctors>
+            <Contract></Contract>
+            <Footer></Footer>
         </>
     );
 };
