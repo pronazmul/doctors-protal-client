6976ec449689a6e8b3b7fa8a4409e843ffd9f7b3
Nazmul Huda <mhnazmul48@gmail.com>
2020-10-09 16:27:29 +0600

    Service Part Done

--- src/Components/Home/Bannar/Bannar.js
+++ src/Components/Home/Bannar/Bannar.js
@@ -7,7 +7,7 @@ const Bannar = () => {
             <div className="container">
                 <div style={{height:'430px'}} className="row d-flex align-items-center">
                     <div className="col-md-4 offset-md-1">
-                        <h1 className='font-weight-bolder' style={{color:'#3a4256'}}>Your New Smile <br/> Starts Here</h1>
+                        <h1 className='font-weight-bolder textColor2'>Your New Smile <br/> Starts Here</h1>
                         <p className='lead'>Vero officia atque ipsa, quo et architecto omnis veniam quos magnam id! Lorem ipsum dolor sit amet.</p>
                         <button className='btn bgGradient'>GET APPOINMENT</button>
                     </div>
--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
+import Service from './Service/Service';
 
 const HomePage = () => {
     return (
         <>
             <Header></Header>
+            <Service></Service>
         </>
     );
 };
